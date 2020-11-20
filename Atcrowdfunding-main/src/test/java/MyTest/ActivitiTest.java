d1415ac775dfbb0c270d933620263c59248fb2be
liu1029957929 <1029957929@qq.com>
2020-11-20 14:36:37 +0800

    2020/11/20（1）

--- Atcrowdfunding-main/src/test/java/MyTest/ActivitiTest.java
+++ Atcrowdfunding-main/src/test/java/MyTest/ActivitiTest.java
@@ -1,24 +1,118 @@
 package MyTest;
 
-import org.activiti.engine.ProcessEngine;
-import org.activiti.engine.RepositoryService;
-import org.activiti.engine.RuntimeService;
-import org.activiti.engine.repository.DeploymentQuery;
+import org.activiti.engine.*;
+import org.activiti.engine.history.HistoricProcessInstance;
+import org.activiti.engine.history.HistoricProcessInstanceQuery;
 import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.repository.ProcessDefinitionQuery;
 import org.activiti.engine.runtime.ProcessInstance;
-import org.activiti.engine.test.Deployment;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.task.TaskQuery;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ActivitiTest {
     ApplicationContext ioc = new ClassPathXmlApplicationContext("spring/spring-*.xml");
     ProcessEngine processEngine = (ProcessEngine) ioc.getBean("processEngine");
 
-    //创建流程实例
+    //执行排他网关任务
+    @Test
+    public  void test10(){
+        TaskService taskService = processEngine.getTaskService();
+        TaskQuery taskQuery = taskService.createTaskQuery();
+/*        List<Task> tasks =  taskQuery.taskAssignee("zhangsan").list();
+        for(Task task:tasks){
+            System.out.println("任务："+task.getName());
+            taskService.complete(task.getId());
+        }*/
+        List<Task> tasks =taskQuery.taskDefinitionKey("_9").list();
+        for(Task task:tasks){
+            System.out.println("任务："+task.getName());
+            taskService.complete(task.getId());
+        }
+    }
+
+    //启动排他网关流程任务
+    @Test
+    public void test09(){
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().latestVersion().singleResult();
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        Map<String,Object> map = new HashMap<>();
+        map.put("day",4);
+        ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinition.getId(),map);
+        System.out.println("processInstance="+processInstance);
+    }
+
+
+    //定义变量
+    @Test
+    public void test08(){
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().latestVersion().singleResult();
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        Map<String,Object> map = new HashMap<>();
+        map.put("tl","zhangsan");
+        map.put("pm","lisi");
+        ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinition.getId(),map);
+        System.out.println("processInstance="+processInstance);
+    }
+
+
+    //任务领取
+    @Test
+    public void test07(){
+        TaskService taskService = processEngine.getTaskService();
+        TaskQuery taskQuery = taskService.createTaskQuery();
+        List<Task> taskList = taskQuery.taskCandidateGroup("team").list();
+        //领取前zhangsna任务数量
+        long count = taskQuery.taskAssignee("zhangsan").count();
+        System.out.println("领取前zhangsan任务数量："+count);
+
+        for(Task task:taskList){
+            taskService.claim(task.getId(),"zhangsan");
+        }
+
+        //领取后zhangsna任务数量
+        TaskQuery taskQuery2 = taskService.createTaskQuery();
+        long count2 = taskQuery2.taskAssignee("zhangsan").count();
+        System.out.println("领取后zhangsan任务数量："+count2);
+    }
+
+    //查询历史流程
+    @Test
+    public void test06(){
+        HistoryService historyService = processEngine.getHistoryService();
+        HistoricProcessInstanceQuery historicProcessInstanceQuery = historyService.createHistoricProcessInstanceQuery();
+        HistoricProcessInstance historicProcessInstance = historicProcessInstanceQuery.processInstanceId("201").finished().singleResult();
+        System.out.println(historicProcessInstance);
+    }
+
+    //执行流程任务
+    @Test
+    public void test05(){
+        TaskService taskService = processEngine.getTaskService();
+        TaskQuery taskQuery = taskService.createTaskQuery();
+        //获取zhangsan 的任务列表
+        List<Task> list = taskQuery.taskAssignee("zhangsan").list();
+        List<Task> list2 = taskQuery.taskAssignee("lisi").list();
+
+        for(Task task:list){
+            System.out.println("zhangsan任务："+task.getName());
+        }
+        System.out.println("-----------------------------");
+        for(Task task:list2){
+            System.out.println("lisi任务："+task.getName());
+            taskService.complete(task.getId());
+        }
+    }
+
+    //启动流程实例
     /*
     * act_hi_actinst,历史的活动的任务表
     * act_hi_procinst,历史的流程实例表
@@ -32,13 +126,12 @@ public class ActivitiTest {
         ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().latestVersion().singleResult();
         RuntimeService runtimeService = processEngine.getRuntimeService();
         ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinition.getId());
-        System.out.println(processInstance);
+        System.out.println("processInstance="+processInstance);
     }
 
     //查询部署流程定义
     @Test
     public void test03(){
-        System.out.println("--------------------");
         RepositoryService repositoryService = processEngine.getRepositoryService();
         ProcessDefinitionQuery query = repositoryService.createProcessDefinitionQuery();
         //查询全部
@@ -59,15 +152,18 @@ public class ActivitiTest {
         System.out.println("Key2="+processDefinition2.getKey());
         System.out.println("Id2="+processDefinition2.getId());
         System.out.println("Version2="+processDefinition2.getVersion());
+
     }
 
     //部署流程定义
     @Test
     public void test02(){
         RepositoryService repositoryService = processEngine.getRepositoryService();
-        repositoryService.createDeployment().addClasspathResource("MyProcess.bpmn").deploy();
+        repositoryService.createDeployment().addClasspathResource("MyProcess5.bpmn").deploy();
+
     }
 
+    //流程引擎对象
     @Test
     public void test01(){
         System.out.println(processEngine);
